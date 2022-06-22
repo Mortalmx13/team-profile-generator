af1fa7eb7b94ae3f21c899fa19386b5f3224c594
Charles Young <ctyoungiv@gmail.com>
2022-06-21 21:12:59 -0400

    get up parent for the different roles

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,4 +1,4 @@
-const Employee = require("../lib/Employee");
+const Employee = require("../lib/employee");
 
 describe("Employee", () => {
 
@@ -6,4 +6,31 @@ describe("Employee", () => {
     const e = new Employee();
     expect(typeof(e)).toBe("object");
   });
+  it("Can set name via constructor arguments" , () => {
+      const name = "rob";
+      const e = new Employee(name);
+      expect(e.name).toBe(name);
+    });
+    it("Can set id via constructor arguments" , () => {
+        const testValue = 100;
+        const e = new Employee("Foo", testValue);
+        expect(e.id).toBe(testValue);
+      });
+      it("Can set email via constructor arguments" , () => {
+        const testValue = "test@question.com";
+        const e = new Employee("Foo", 1 , testValue);
+        expect(e.email).toBe(testValue);
+      });
+      it("Can get name via getName()" , () =>{
+        expect(e.getName()).toBe('rob')
+      });
+      it("Can get id via getId()" , () =>{
+        expect(e.getId()).toBe('1')
+      });
+      it("Can get name via getEmail()" , () =>{
+        expect(e.getEmail()).toBe('test@question.com')
+      });
+      it("Can ger role via getRole()", () =>{
+        expect(e.getRole()).toBe('Employee')
+      });
 });
