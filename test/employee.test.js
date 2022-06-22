297bd76d21578d6b0fc2438ac5e183f3a9c28096
Charles Young <ctyoungiv@gmail.com>
2022-06-21 21:41:30 -0400

    found a shorter way to write out the test.js 's and finished libs

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,36 +1,21 @@
 const Employee = require("../lib/employee");
-
+const employee = new Employee('rob', '1', 'test@question.com')
 describe("Employee", () => {
-
-  it("Can instantiate Employee instance", () => {
-    const e = new Employee();
-    expect(typeof(e)).toBe("object");
-  });
-  it("Can set name via constructor arguments" , () => {
-      const name = "rob";
-      const e = new Employee(name);
-      expect(e.name).toBe(name);
-    });
-    it("Can set id via constructor arguments" , () => {
-        const testValue = 100;
-        const e = new Employee("Foo", testValue);
-        expect(e.id).toBe(testValue);
-      });
-      it("Can set email via constructor arguments" , () => {
-        const testValue = "test@question.com";
-        const e = new Employee("Foo", 1 , testValue);
-        expect(e.email).toBe(testValue);
-      });
+it("Can get values for employee", () =>{
+    expect(employee.name).toBe('rob');
+    expect(employee.id).toBe('1');
+    expect(employee.email).toBe('test@question.com');
+})
       it("Can get name via getName()" , () =>{
-        expect(e.getName()).toBe('rob')
+        expect(employee.getName()).toBe('rob')
       });
       it("Can get id via getId()" , () =>{
-        expect(e.getId()).toBe('1')
+        expect(employee.getId()).toBe('1')
       });
       it("Can get name via getEmail()" , () =>{
-        expect(e.getEmail()).toBe('test@question.com')
+        expect(employee.getEmail()).toBe('test@question.com')
       });
-      it("Can ger role via getRole()", () =>{
-        expect(e.getRole()).toBe('Employee')
+      it("Can get role via getRole()", () =>{
+        expect(employee.getRole()).toBe('Employee')
       });
 });
